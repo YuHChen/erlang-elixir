1db123ab754c8495b7742a429d98b36263550c60
Alexander Strong <alex.j.strong@gmail.com>
2016-03-17 13:47:49 -0400

    added a bit of pipe stuff

---
+++ C demo/teacher_student/classDemo.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+
+#define MAX_LEN 128
+#define N 4
+
+int isOdd(int n) {
+  return n%2;
+}
+
+int main() {
+  pid_t pid;
+  int i;
+  char buf[MAX_LEN];
+  //sending messages from parent to child
+  int parentToChild[2];
+  pipe(parentToChild);
+  //sending messages from child to parent
+  int childToParent[2];
+  pipe(childToParent);
+  pid = fork();
+  if(pid < 0) exit(pid);
+  if(pid == 0) {
+    printf("Teacher\n");
+    close(parentToChild[1]);
+    close(childToParent[0]);
+    pid_t pids[N];
+    for(i = 0; i < N; i++) {
+      pids[i] = fork();
+      if(pids[i] < 0) exit(pid);
+    }
+  }
+  else {
+    printf("Student\n");
+    close(parentToChild[0]);
+    close(childToParent[1]);
+  }
+}
