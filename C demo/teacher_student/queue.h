9b98dc6f4727bb9ef707a578060f5a836e7ba135
Alexander Strong <alex.j.strong@gmail.com>
2016-03-17 13:26:43 -0400

    added another queue thing

---
+++ C demo/teacher_student/queue.h
@@ -0,0 +1,11 @@
+typedef struct stringnode {
+  char *data;
+  struct stringnode *next;
+} node;
+
+typedef struct fifoqueue {
+  node *head, *tail;
+  //int size;
+  int (*add)(struct fifoqueue *self, char *s);
+  char* (*remove)(struct fifoqueue *self);
+} queue;
